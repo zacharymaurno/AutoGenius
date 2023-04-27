792302ea9bb82312ad490441be0fdaad6f88588b
zacharymaurno <zmaurno@gmail.com>
2023-04-27 19:19:37 -0400

    update

--- Car.h
+++ Car.h
@@ -14,14 +14,10 @@ private:
     int mileage;
     string color;
     int price;
-    double safetyFactor;
-    //double totalScore = 0;
 public:
     double totalScore;
     Car(string make,string model,string frame, int year, int mileage, string color, int price);
     void getTotalScore(string preferredFrame, string preferredColor, int maxPrice);
-    void bruteForce();
-    void dijkstra();
     void PrintInfo();
 
 };
